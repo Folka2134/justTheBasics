95e3b8aa130a148e973383ba841a3da013d5f7ae
Folka2134 <folka2134@gmail.com>
2022-03-02 19:37:19 +0000

    Implemented navbar routing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { Routes, Route, Link } from "react-router-dom";
 
 import { Home } from "./components/Home";
 import { Todo } from "./components/Todo/Todo";
-
-// import { Navbar } from "./components/Navbar";
+import { Game } from "./components/Game/Game";
+import { Weather } from "./components/Weather/Weather";
+import { News } from "./components/News/News";
 
 import logo from "./images/justTheBasicsSmall.png"
 
@@ -15,17 +16,20 @@ const App = () => {
           <Link to="/"><img src={logo} alt="" className='h-14' /></Link>
           <ul className='h-full flex p-2'>
             <Link to="/todo"><li className='p-2 underline'>CRUD-app</li></Link>
-            <li className='p-2 underline'>React Game</li>
-            <li className='p-2 underline'>Api Weather</li>
-            <li className='p-2 underline'>Api News</li>
+            <Link to="/game"><li className='p-2 underline'>React Game</li></Link>
+            <Link to="/weather"><li className='p-2 underline'>Api Weather</li></Link>
+            <Link to="/news"><li className='p-2 underline'>Api News</li></Link>
           </ul>
         </div>
       </div>
       <div className='h-10 w-full grid bg-white border-b-2 justify-center content-center'><p className='text-sm'>Compilation of basic projects displaying a strong foundation.</p></div>
-      {/* <Navbar /> */}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo" element={<Todo />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/news" element={<News />} />
       </Routes>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>
-      <>
-        Home
-      </>
+    <div className='h-screen bg-purple-700 grid justify-center content-center'>
+      <div className='h-[500px] w-[400px] grid grid-rows-2 grid-cols-2 border bg-green-2'>
+        <div className='border'>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
     </div>
   )
 }
