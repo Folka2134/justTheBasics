34424ccbeaecd8b295905bd934e765958eca73bd
Folka2134 <folka2134@gmail.com>
2022-03-02 19:12:46 +0000

    Implementing react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import { Navbar } from "./components/Navbar";
+import { Routes, Route, Link } from "react-router-dom";
+
+import { Home } from "./components/Home";
+import { Todo } from "./components/Todo/Todo";
+
+// import { Navbar } from "./components/Navbar";
 
 import logo from "./images/justTheBasicsSmall.png"
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <div className='h-14 w-full bg-white border-b-2 truncate'>
+        <div className='flex'>
+          <Link to="/"><img src={logo} alt="" className='h-14' /></Link>
+          <ul className='h-full flex p-2'>
+            <Link to="/todo"><li className='p-2 underline'>CRUD-app</li></Link>
+            <li className='p-2 underline'>React Game</li>
+            <li className='p-2 underline'>Api Weather</li>
+            <li className='p-2 underline'>Api News</li>
+          </ul>
+        </div>
+      </div>
+      <div className='h-10 w-full grid bg-white border-b-2 justify-center content-center'><p className='text-sm'>Compilation of basic projects displaying a strong foundation.</p></div>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Routes, Route, Link } from "react-router-dom";
 
-import App from '../App';
+// import App from '../App';
+import { Todo } from './Todo/Todo';
 
 import logo from "../images/justTheBasicsSmall.png"
 
@@ -10,13 +11,9 @@ export const Navbar = () => {
     <>
       <div className='h-14 w-full bg-white border-b-2 truncate'>
         <div className='flex'>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/" element={<Todo />} />
-          </Routes>
-          <img src={logo} alt="" className='h-14' />
+          <Link to="/"><img src={logo} alt="" className='h-14' /></Link>
           <ul className='h-full flex p-2'>
-            <li className='p-2 underline'>CRUD-app</li>
+            <Link to="/todo"><li className='p-2 underline'>CRUD-app</li></Link>
             <li className='p-2 underline'>React Game</li>
             <li className='p-2 underline'>Api Weather</li>
             <li className='p-2 underline'>Api News</li>
@@ -24,6 +21,10 @@ export const Navbar = () => {
         </div>
       </div>
       <div className='h-10 w-full grid bg-white border-b-2 justify-center content-center'><p className='text-sm'>Compilation of basic projects displaying a strong foundation.</p></div>
+      <Routes>
+        {/* <Route path="*" element={ } /> */}
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
     </>
   )
 }
