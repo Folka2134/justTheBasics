7bedffd78e56ee9940eeac2337966da68a4a3520
Folka2134 <folka2134@gmail.com>
2022-03-02 20:22:29 +0000

    Imeplemented deleteTodo functionality

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { GlobalContext } from './context/GlobalState'
 
 export const TodoItem = ({ todo }) => {
+  const { deleteTodo } = useContext(GlobalContext)
+
   return (
-    <li className='flex justify-between'><span>{todo.todo}</span><button>x</button></li>
+    <li className='flex justify-between'><span>{todo.todo}</span><button onClick={() => deleteTodo(todo.id)}>x</button></li>
   )
 }
