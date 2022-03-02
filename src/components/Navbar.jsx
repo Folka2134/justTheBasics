03af456b06515342fcef06e88595166518b753a5
Folka2134 <folka2134@gmail.com>
2022-03-02 17:58:09 +0000

    Added logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import logo from "../images/justTheBasicsSmall.png"
+
 export const Navbar = () => {
   return (
     <div className='h-14 w-full bg-white border-b-2'>
-      <div>
-        <img src="" alt="" />
+      <div className='flex'>
+        <img src={logo} alt="" className='h-14' />
         <ul className='h-full flex p-2'>
           <li className='p-2'>test</li>
           <li className='p-2'>test</li>
