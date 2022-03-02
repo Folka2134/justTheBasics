6a77e1100918ca6c4b941edd2f9e5319921f5cbf
Folka2134 <folka2134@gmail.com>
2022-03-02 21:20:15 +0000

    Added checkbox to TodoItem

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -6,6 +6,13 @@ export const TodoItem = ({ todo }) => {
   const { deleteTodo } = useContext(GlobalContext)
 
   return (
-    <li className='flex justify-between'><span>{todo.todo}</span><button onClick={() => deleteTodo(todo.id)}>x</button></li>
+    // <li className='flex justify-between'><span>{todo.todo}</span><button onClick={() => deleteTodo(todo.id)}>x</button></li>
+    <li className='flex justify-between'>
+      <span>
+        <input type="checkbox" value={todo.id} className="m-1" />
+        <label htmlFor={todo.id} className="active:line-through">{todo.todo}</label>
+      </span>
+      <button onClick={() => deleteTodo(todo.id)} className="m-1">x</button>
+    </li>
   )
 }
