514ec5e40d0a9629a2620139dd0925eb64b5d22b
Folka2134 <folka2134@gmail.com>
2022-03-02 20:13:37 +0000

    Updated listItem styles

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const TodoItem = ({ todo }) => {
   return (
-    <li><span>{todo.todo}</span><button>x</button></li>
+    <li className='flex justify-between'><span>{todo.todo}</span><button>x</button></li>
   )
 }
