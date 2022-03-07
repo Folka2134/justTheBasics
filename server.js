21954e51691819d50752fbfe0ff45bfb56a1c1eb
Folka2134 <folka2134@gmail.com>
2022-03-07 18:41:20 +0000

    Create basic route

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+const todos = require('./routes/todos')
+
 dotenv.config({ path: './config/config.env'})
 
 const app = express()
 const PORT = process.env.PORT
 
-app.get('/', (req, res) => res.send('testtest'))
+app.use('/api/v1/todos', todos)
+
+// app.get('/', (req, res) => res.send('testtest'))
 
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on PORT:${PORT}`))
