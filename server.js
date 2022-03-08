5c776d2a7ff842ac054b56a64895cbac88a70fd9
Folka2134 <folka2134@gmail.com>
2022-03-08 17:42:21 +0000

    Cleaned up comments

--- server.js
+++ server.js
@@ -10,6 +10,4 @@ const PORT = process.env.PORT
 
 app.use('/api/v1/todos', todos)
 
-// app.get('/', (req, res) => res.send('testtest'))
-
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on PORT:${PORT}`))
