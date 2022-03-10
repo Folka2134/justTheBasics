d027c6502fb1b9adafcc11bdea9f908da7d306e2
Folka2134 <folka2134@gmail.com>
2022-03-10 14:38:17 +0000

    Renamed todo  model

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,12 +1,12 @@
 // Bring in Todo Model
-const TodosModel = require('../models/TodosModel')
-const Todo = require('../models/TodosModel')
+
+const Todos = require('../models/Todos')
 
 // @desc  GET all todos
 // @route GET /api/v1/todos
 exports.getTodos = async (req, res, next) => {
   try {
-    const todos = await TodosModel.find()
+    const todos = await Todos.find()
     return res.status(200).json({
       success: true,
       data: todos
@@ -21,8 +21,28 @@ exports.getTodos = async (req, res, next) => {
 
 // @desc Add todo
 // @route POST /api/v1/todos
-exports.addTodo = (req, res, next) => {
-  res.send('POST todo')
+exports.addTodo = async (req, res, next) => {
+  try {
+    const { text } = req.body
+    const todo = await Todos.create(req.body)
+    return res.status(200).json({
+      success: true,
+      data: todo
+    })
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      const messages = Object.values(err.errors).map((val) => val.message)
+      return res.status(400).json({
+        success: false,
+        error: messages
+      })
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: 'Server Error'
+      })
+    }
+  }
 }
 
 // @desc Delete todo
