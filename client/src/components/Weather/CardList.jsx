d625b07634927600ac4e45889fa64385f248a75f
Folka2134 <folka2134@gmail.com>
2022-03-11 15:21:06 +0000

    Created CardList with cards

---
+++ client/src/components/Weather/CardList.jsx
@@ -0,0 +1,162 @@
+import React from 'react'
+
+export const CardList = () => {
+  return (
+    <div className='grid justify-center overflow-hidden'>
+      <ul className='flex justify-center'>
+        <li className='bg-purple-500 h-96 w-64 mr-6'>
+          <div className='w-64 p-6 absolute flex justify-between'>
+            <p className='text-cyan-400'>Icon1</p>
+            <p className='text-cyan-400'>Icon2</p>
+          </div>
+          <img src="#" alt="testimage" className='bg-black h-2/3' />
+          <h1 className='text-lg border-black text-center'>Country</h1>
+          <h2 className='border-b-2 border-black text-center'>City</h2>
+          <div className='flex justify-evenly'>
+            <div className='w-full border-r-2 border-black p-1 '>
+              <p>data</p>
+              <p>data</p>
+              <p>data</p>
+            </div>
+            <div className='w-full p-1'>
+              <p>data</p>
+            </div>
+          </div>
+        </li>
+        <li className='bg-purple-500 h-96 w-64 mr-6'>
+          <div className='w-64 p-6 absolute flex justify-between'>
+            <p className='text-cyan-400'>Icon1</p>
+            <p className='text-cyan-400'>Icon2</p>
+          </div>
+          <img src="#" alt="testimage" className='bg-black h-2/3' />
+          <h1 className='text-lg border-black text-center'>Country</h1>
+          <h2 className='border-b-2 border-black text-center'>City</h2>
+          <div className='flex justify-evenly'>
+            <div className='w-full border-r-2 border-black p-1 '>
+              <p>data</p>
+              <p>data</p>
+              <p>data</p>
+            </div>
+            <div className='w-full p-1'>
+              <p>data</p>
+            </div>
+          </div>
+        </li>
+        <li className='bg-purple-500 h-96 w-64 mr-6'>
+          <div className='w-64 p-6 absolute flex justify-between'>
+            <p className='text-cyan-400'>Icon1</p>
+            <p className='text-cyan-400'>Icon2</p>
+          </div>
+          <img src="#" alt="testimage" className='bg-black h-2/3' />
+          <h1 className='text-lg border-black text-center'>Country</h1>
+          <h2 className='border-b-2 border-black text-center'>City</h2>
+          <div className='flex justify-evenly'>
+            <div className='w-full border-r-2 border-black p-1 '>
+              <p>data</p>
+              <p>data</p>
+              <p>data</p>
+            </div>
+            <div className='w-full p-1'>
+              <p>data</p>
+            </div>
+          </div>
+        </li>
+        <li className='bg-purple-500 h-96 w-64 mr-6'>
+          <div className='w-64 p-6 absolute flex justify-between'>
+            <p className='text-cyan-400'>Icon1</p>
+            <p className='text-cyan-400'>Icon2</p>
+          </div>
+          <img src="#" alt="testimage" className='bg-black h-2/3' />
+          <h1 className='text-lg border-black text-center'>Country</h1>
+          <h2 className='border-b-2 border-black text-center'>City</h2>
+          <div className='flex justify-evenly'>
+            <div className='w-full border-r-2 border-black p-1 '>
+              <p>data</p>
+              <p>data</p>
+              <p>data</p>
+            </div>
+            <div className='w-full p-1'>
+              <p>data</p>
+            </div>
+          </div>
+        </li>
+        <li className='bg-purple-500 h-96 w-64 mr-6'>
+          <div className='w-64 p-6 absolute flex justify-between'>
+            <p className='text-cyan-400'>Icon1</p>
+            <p className='text-cyan-400'>Icon2</p>
+          </div>
+          <img src="#" alt="testimage" className='bg-black h-2/3' />
+          <h1 className='text-lg border-black text-center'>Country</h1>
+          <h2 className='border-b-2 border-black text-center'>City</h2>
+          <div className='flex justify-evenly'>
+            <div className='w-full border-r-2 border-black p-1 '>
+              <p>data</p>
+              <p>data</p>
+              <p>data</p>
+            </div>
+            <div className='w-full p-1'>
+              <p>data</p>
+            </div>
+          </div>
+        </li>
+        <li className='bg-purple-500 h-96 w-64 mr-6'>
+          <div className='w-64 p-6 absolute flex justify-between'>
+            <p className='text-cyan-400'>Icon1</p>
+            <p className='text-cyan-400'>Icon2</p>
+          </div>
+          <img src="#" alt="testimage" className='bg-black h-2/3' />
+          <h1 className='text-lg border-black text-center'>Country</h1>
+          <h2 className='border-b-2 border-black text-center'>City</h2>
+          <div className='flex justify-evenly'>
+            <div className='w-full border-r-2 border-black p-1 '>
+              <p>data</p>
+              <p>data</p>
+              <p>data</p>
+            </div>
+            <div className='w-full p-1'>
+              <p>data</p>
+            </div>
+          </div>
+        </li>
+        <li className='bg-purple-500 h-96 w-64 mr-6'>
+          <div className='w-64 p-6 absolute flex justify-between'>
+            <p className='text-cyan-400'>Icon1</p>
+            <p className='text-cyan-400'>Icon2</p>
+          </div>
+          <img src="#" alt="testimage" className='bg-black h-2/3' />
+          <h1 className='text-lg border-black text-center'>Country</h1>
+          <h2 className='border-b-2 border-black text-center'>City</h2>
+          <div className='flex justify-evenly'>
+            <div className='w-full border-r-2 border-black p-1 '>
+              <p>data</p>
+              <p>data</p>
+              <p>data</p>
+            </div>
+            <div className='w-full p-1'>
+              <p>data</p>
+            </div>
+          </div>
+        </li>
+        <li className='bg-purple-500 h-96 w-64 mr-6'>
+          <div className='w-64 p-6 absolute flex justify-between'>
+            <p className='text-cyan-400'>Icon1</p>
+            <p className='text-cyan-400'>Icon2</p>
+          </div>
+          <img src="#" alt="testimage" className='bg-black h-2/3' />
+          <h1 className='text-lg border-black text-center'>Country</h1>
+          <h2 className='border-b-2 border-black text-center'>City</h2>
+          <div className='flex justify-evenly'>
+            <div className='w-full border-r-2 border-black p-1 '>
+              <p>data</p>
+              <p>data</p>
+              <p>data</p>
+            </div>
+            <div className='w-full p-1'>
+              <p>data</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
--- client/src/components/Weather/Weather.jsx
+++ client/src/components/Weather/Weather.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { CardList } from './CardList'
 
 export const Weather = () => {
   return (
-    <div className='h-screen bg-blue-800 grid'>
-      <div className='m-8 mt-16 bg-blue-900 bg-opacity-80'>
-        test
+    <div className='h-full min-h-screen bg-blue-800 flex'>
+      <div className='flex m-8 mt-16 bg-blue-900 bg-opacity-80'>
+        <CardList />
       </div>
     </div>
   )
