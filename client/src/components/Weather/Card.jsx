8defac0b09832534f913407881491a1982d5996b
Folka2134 <folka2134@gmail.com>
2022-03-11 16:02:42 +0000

    Updated styles

--- client/src/components/Weather/Card.jsx
+++ client/src/components/Weather/Card.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const Card = () => {
   return (
     <>
-      <li className='bg-[#1D18C7] h-96 w-64 m-6 overflow-clip'>
+      <li className='bg-[#1D18C7] h-96 w-64 m-6 overflow-hidden rounded-lg'>
         <div className='w-64 p-6 absolute flex justify-between'>
-          <p className='bg-[#1D18C7] border-2 border-cyan-400 p-4'>Icon1</p>
-          <p className='bg-[#1D18C7] border-2 border-cyan-400 p-4'>Icon2</p>
+          <p className='bg-[#1D18C7] border-2 border-cyan-400 p-4 rounded-lg'>Icon1</p>
+          <p className='bg-[#1D18C7] border-2 border-cyan-400 p-4 rounded-lg'>Icon2</p>
         </div>
         <img src="https://media.architecturaldigest.com/photos/56fd9d65ecd154e0329c1627/4:3/w_768/london-travel-guide-lede.jpg" alt="testimage" className='bg-black h-2/3' />
         <h1 className='text-lg border-black text-center'>Country</h1>
@@ -14,11 +14,11 @@ export const Card = () => {
         <div className='flex justify-evenly'>
           <div className='w-full border-r-2 border-black p-1 '>
             <p className='text-sm'>Windspeed: 5mph</p>
-            <p>data</p>
-            <p>data</p>
+            <p className='text-sm'>Windspeed: 5mph</p>
+            <p className='text-sm'>Windspeed: 5mph</p>
           </div>
           <div className='w-full p-1'>
-            <p>data</p>
+            <p className='text-sm'>Windspeed: 5mph</p>
           </div>
         </div>
       </li>
