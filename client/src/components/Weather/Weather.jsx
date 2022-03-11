0467ad2ad6a431cba0a00dd3651107d346eb3a65
Folka2134 <folka2134@gmail.com>
2022-03-11 16:43:12 +0000

    Displayed input field

--- client/src/components/Weather/Weather.jsx
+++ client/src/components/Weather/Weather.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { CardList } from './CardList'
+import { SearchBox } from './SearchBox'
 
 export const Weather = () => {
   return (
     <div className='h-full min-h-screen bg-blue-800 flex'>
-      <div className='flex h-full m-8 mt-16 bg-blue-900 bg-opacity-80'>
+      <div className="h-full m-8 mt-16 ">
+        <SearchBox />
+        history
+      </div>
+      <div className='flex h-full m-8 mt-32 bg-blue-900 bg-opacity-80'>
         <CardList />
       </div>
-    </div>
+    </div >
   )
 }
