b76c39483eb3268d70f90571697fcb56091526cd
Folka2134 <folka2134@gmail.com>
2022-03-11 14:43:26 +0000

    Updated Weather app

--- client/src/components/Weather/Weather.jsx
+++ client/src/components/Weather/Weather.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Weather = () => {
   return (
-    <div>Weather</div>
+    <div className='h-screen bg-blue-800 grid justify-center'>
+      <div className='mt-16 bg-blue-900 bg-opacity-80'>
+        weather
+      </div>
+    </div>
   )
 }
