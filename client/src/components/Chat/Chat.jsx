d0c3271112f46d79cb22c5a92ef4a05fae07d0c9
Folka2134 <folka2134@gmail.com>
2022-03-22 12:00:18 +0000

    Added firebase boilerplate

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,6 +1,35 @@
 import React from 'react'
+import firebase from 'firebase/compat/app'
+
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDGseTVycB4ACXhR237e68ZcRP8PG5Bzmw",
+
+  authDomain: "super-chat-8ace6.firebaseapp.com",
+
+  projectId: "super-chat-8ace6",
+
+  storageBucket: "super-chat-8ace6.appspot.com",
+
+  messagingSenderId: "339361691952",
+
+  appId: "1:339361691952:web:3c0dcdd3dfe3ff9ed670f5",
+
+  measurementId: "G-YXRKT6DWHH"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 export const Chat = () => {
+
+  const [user] = useAuthState(auth)
+
   return (
     <div className='h-screen grid bg-indigo-800 justify-center'>
       <div className='h-[56rem] w-96 mt-16 bg-slate-100 self-center'>
@@ -17,9 +46,9 @@ export const Chat = () => {
   )
 }
 
-function SignIn() {
+// function SignIn() {
 
-}
+// }
 
 // function SignOut() {
 
