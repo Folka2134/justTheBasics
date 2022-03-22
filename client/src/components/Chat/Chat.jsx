45d343106618a8c7fed4f4f471cc40bb7d412ac8
Folka2134 <folka2134@gmail.com>
2022-03-22 11:53:15 +0000

    Implemented styles

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className='h-screen grid bg-indigo-800 justify-center'>
+      <div className='h-[56rem] w-96 mt-16 bg-slate-100 self-center'>
+        <header className='h-16 w-full bg-gray-800 text-4xl text-[#759BD1] inline-block p-2'>
+          Chat!
+          {/* <SignOut /> */}
+        </header>
+        <div>
+          <div className='bg-pink-100 h-full'>area</div>
+          {/* user? <ChatRoom /> : <SignIn /> */}
+        </div>
+      </div>
+    </div>
   )
 }
