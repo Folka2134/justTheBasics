347e090212419dcc4c413976fbf7372ac90a72f4
Folka2134 <folka2134@gmail.com>
2022-03-24 12:18:35 +0000

    added firebase boilerplate

--- client/src/components/Todo/Todo.jsx
+++ client/src/components/Todo/Todo.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
 import { Input } from './Input'
 import { List } from './List'
 
 import { GlobalProvider } from './context/GlobalState'
+
+firebase.initializeApp({
+
+  apiKey: "AIzaSyDy8JADVgZDaSoNuswUXI0jLVQMoJ61sKU",
+
+  authDomain: "todo-list-f2c15.firebaseapp.com",
+
+  projectId: "todo-list-f2c15",
+
+  storageBucket: "todo-list-f2c15.appspot.com",
+
+  messagingSenderId: "637670506803",
+
+  appId: "1:637670506803:web:39aac6eee7056d4158fe46"
+
+})
+
+const firestore = firebase.firestore()
 
 export const Todo = () => {
   return (
